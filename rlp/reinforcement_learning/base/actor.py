f2e0767292d99a5d28bd733ae45ab6e49c0d367d
DESKTOP-EI5LPJS\Shem <shauns4y@gmail.com>
2024-05-13 22:26:35 +0800

    Add BaseActor class and it's design

---
+++ rlp/reinforcement_learning/base/actor.py
@@ -0,0 +1,10 @@
+import abc
+
+
+class BaseActor(abc.ABC):
+    """BaseActor defines the basic functionalities of an agent in a reinforcement learning environment.
+
+    This abstract base class should be extended by any specific actor implementations that define
+    how the agent chooses actions and potentially learns from experiences."""
+
+    pass
